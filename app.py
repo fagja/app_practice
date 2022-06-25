c42f521efe1486fdbb508687ef11cf4752026ad4
fagja <hey.shunya19@gmail.com>
2022-06-25 09:41:20 +0900

    実行ファイル

---
+++ app.py
@@ -0,0 +1,11 @@
+import random
+import time
+
+omikuji = ["大吉", "吉", "凶", "中吉"]
+
+result = random.choice(omikuji)
+
+print(f"今日の運勢は... {result}")
+
+# Windowsは処理終了時にターミナルが閉じてしまうので、`sleep`などを追加する
+time.sleep(5)
